e4823b5e37c3e6655e7bc84889e0556f022b5d33
Ruslan Nurnazarov <ruslan7692@gmail.com>
2025-10-07 16:35:36 +0400

    refactor

--- src/main/java/com/example/javajpaplayground/domain/entity/Document.java
+++ src/main/java/com/example/javajpaplayground/domain/entity/Document.java
@@ -3,6 +3,7 @@ package com.example.javajpaplayground.domain.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -48,7 +49,7 @@ public class Document {
     @Column(name = "created_at")
     private Instant createdAt;
 
-    @OneToOne(mappedBy = "document")
+    @OneToOne(mappedBy = "document", fetch = FetchType.LAZY)
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
     @JsonIgnore
